a2a5ee6d02f5362cdbb65e722cd9b105701e873a
ecwsa <edwardsavin@cmail.carleton.ca>
2025-09-15 17:04:32 -0400

    x

---
+++ Users/ecwsa/IdeaProjects/Lab1_SYSC3110/src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class AddressBook
+{
+    private List<BuddyInfo> buddies;
+    public  AddressBook(){
+        this.buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        if (this.buddies.contains(buddy)){
+            System.out.println("Buddy " + buddy.getName() + " already exists");
+        }
+        this.buddies.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        this.buddies.remove(buddy);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("AddressBook");
+    }
+}
