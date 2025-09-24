85758f1a34f998835748d328b06fb512a3abf2ff
ecwsa <edwardsavin@cmail.carleton.ca>
2025-09-24 14:47:08 -0400

    Y1

---
+++ Users/ecwsa/IdeaProjects/Lab1_SYSC3110/src/BuddyInfo.java
@@ -0,0 +1,32 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private int phoneNumber;
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BuddyInfo() {
+        this("Bob", "Carleton University", 888634);
+    }
+
+    public BuddyInfo(String name, String address, int phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo Elmo = new BuddyInfo("Elmo", "Sesame Street", 123456789);
+        System.out.println("Hello " + Elmo.getName());
+    }
+}
